fe7109017f4b8da72f0e0d91fbc51a0c7511dea6
jeong won <jw221356@naver.com>
2023-12-26 11:10:59 +0900

    12_26 수업

---
+++ lib/23_12_26/routes.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter_beginner_class/23_12_26/01_go_router.dart';
+import 'package:go_router/go_router.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const StartScreen(),
+    ),
+    GoRoute(
+      path: '/end',
+      builder: (context, state) {
+        final person =
+        Person.fromJson(jsonDecode(state.uri.queryParameters['사람']!));
+
+        return EndScreen(person: person);
+      },
+    ),
+  ],
+);
